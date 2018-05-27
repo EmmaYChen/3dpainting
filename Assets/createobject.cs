2844480985ae86c08922a448c4c8278d1275f373
emma <yaochen.jo@gmail.com>
2018-05-26 21:38:02 -0700

    create object

---
+++ Assets/createobject.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class createobject : MonoBehaviour {
+
+    public GameObject cube;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger)){
+            Debug.Log("create an cube");
+            Instantiate(cube, transform.position, transform.rotation);
+        }
+		
+	}
+}
