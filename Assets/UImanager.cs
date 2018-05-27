f0da18dd980fa6811c0b7215167768e2094da986
emma <yaochen.jo@gmail.com>
2018-05-26 23:32:52 -0700

    button click

---
+++ Assets/UImanager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UImanager : MonoBehaviour {
+    private GameObject color_panel;
+
+    // Use this for initialization
+    void Start()
+    {
+        color_panel = GameObject.FindGameObjectWithTag("colorpanel");
+        color_panel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Button color_button = gameObject.GetComponent<Button>();
+        //if(color_button.onClick()){
+            
+        //}
+        //if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
+        //{
+        //    Debug.Log("color button click");
+
+        //}
+    }
+
+    public void Color_panel_showup()
+    {
+        color_panel.SetActive(true);   
+    }
+}
--- Assets/createobject.cs
+++ Assets/createobject.cs
@@ -15,10 +15,23 @@ public class createobject : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger)){
-            Debug.Log("create an cube");
-            Instantiate(cube, transform.position, transform.rotation);
-        }
+         ////old way
+         //if (Input.GetMouseButton(0)) // the finger has touched the HMD trackpad.
+         //{
+         //    Vector2 touchPosition = Input.mousePosition;
+         //    Debug.Log("create an cube");
+         //    Instantiate(cube, transform.position, transform.rotation);
+         //}
+
+
+        //if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger)){
+        //    Debug.Log("create an cube");
+        //    Instantiate(cube, transform.position, transform.rotation);
+        //}
+
+
+
+
 		
 	}
 }
