563ddfdb5c16fb4f20053e0f4bf76c92d70231be
emma <yaochen.jo@gmail.com>
2018-05-28 18:46:33 -0700

    newest 6:30pm

--- Assets/Script/UImanager.cs
+++ Assets/Script/UImanager.cs
@@ -5,12 +5,14 @@ using UnityEngine.UI;
 
 public class UImanager : MonoBehaviour {
     private GameObject color_panel;
+    public GameObject test;
 
     // Use this for initialization
     void Start()
     {
         color_panel = GameObject.FindGameObjectWithTag("colorpanel");
         color_panel.gameObject.SetActive(false);
+        test.gameObject.SetActive(false); 
     }
 
     // Update is called once per frame
@@ -20,15 +22,21 @@ public class UImanager : MonoBehaviour {
         //if(color_button.onClick()){
             
         //}
-        //if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
-        //{
-        //    Debug.Log("color button click");
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
+        {
+            Debug.Log("color button click");
 
-        //}
+        }
     }
 
     public void Color_panel_showup()
     {
         color_panel.gameObject.SetActive(true);   
+         
+    }
+
+    public void Shapes_panel_showup()
+    {
+        test.gameObject.SetActive(true);
     }
 }
