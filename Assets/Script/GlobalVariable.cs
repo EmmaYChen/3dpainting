c7da63460412b1aefd8f32d4499f671abed5d62e
Douphoton <jiangjq@stanford.edu>
2018-05-30 00:24:58 -0700

    add global varible and set grab state

---
+++ Assets/Script/GlobalVariable.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class GlobalVariable : MonoBehaviour {
+
+	static public string MODE = "None";
+	// Use this for initialization
+	void Start () {
+		MODE = "None";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Script/SetGrabState.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetGrabState : MonoBehaviour {
+
+
+	public void SetGrab()
+	{
+		if (GlobalVariable.MODE == "Grab_On"){
+			GlobalVariable.MODE = "Grab_Off";
+		}
+		else
+		{
+			GlobalVariable.MODE = "Grab_On";
+		}
+	}
+}
